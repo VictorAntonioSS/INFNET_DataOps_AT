2283b0ab23cf3506afc36565262cea02aa3ed503
Victor Antonio <victor.ssantos@al.infnet.edu.br>
2026-06-22 21:36:48 +0000

    Fato_vendas convertida de view para table

--- dbt/models/fato_vendas.sql
+++ dbt/models/fato_vendas.sql
@@ -1,15 +1,17 @@
+{{ config(materialized='table') }}
+
 select
     cast(i.id_item as integer) as id_item,
     cast(i.id_pedido as integer) as id_pedido,
     cast(p.id_cliente as integer) as id_cliente,
-    c.cpf as cpf,
     cast(i.id_produto as integer) as id_produto,
     cast(i.quantidade as integer) as quantidade,
     cast(i.valor_unitario as double) as valor_unitario,
     cast(i.valor_total_item as double) as valor_total_item,
-    cast(p.data_pedido as date) as data_pedido
+    cast(p.data_pedido as date) as data_pedido,
+    c.cpf
 from {{ source('silver', 'itens_pedido') }} i
 left join {{ source('silver', 'pedidos') }} p
     on cast(i.id_pedido as integer) = cast(p.id_pedido as integer)
-left join {{ source('silver', 'clientes') }} c
-    on cast(p.id_cliente as integer) = cast(c.id_cliente as integer)
+left join {{ ref('dim_cliente') }} c
+    on cast(p.id_cliente as integer) = c.id_cliente
